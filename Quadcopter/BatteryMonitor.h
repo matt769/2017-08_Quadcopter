89a3808ee3ff1c40c5ec60f97d1c58212eded06b
matt769 <matthedley@hotmail.co.uk>
2017-10-21 23:26:59 +0100

    tidied up battery monitor tab a bit more

--- Quadcopter/BatteryMonitor.h
+++ Quadcopter/BatteryMonitor.h
@@ -4,11 +4,11 @@
 // 1024 / 5 * 3.907 = 800 at max voltage
 // 1024 / 5 * 3.3 = 676 at 3.3V
 // 1024 / 5 * 3.1 = 635 REALLY NEED TO STOP NOW (automatic land?)
-
 // current through 3.3k resistor will be 3.907 / 3300 = 1mA
 
-const byte PIN_BATTERY_MONITOR = A0;
-const byte PIN_BATTERY_INDICATOR = 8;
+// the battery level variable is used automatically when building the acknowledgement payload
+
+const byte pinBatteryMonitor = A0;
 const float batteryFilterAlpha = 0.5;
 int dividerReading = 0;
 byte batteryLevel = 0;
@@ -16,48 +16,20 @@ const int dividerMaxReading = 250; // corresponds to full charge, 16.8V
 const int dividerMinReading = 100;  // minimum that the battery should ever get to
 const int dividerRange = dividerMaxReading - dividerMinReading;
 
-
 void calculateBatteryLevel() {
-  int tmp = analogRead(PIN_BATTERY_MONITOR);
+  int tmp = analogRead(pinBatteryMonitor);
   dividerReading = (1-batteryFilterAlpha) * dividerReading + batteryFilterAlpha * tmp;
   float batteryLevelFloat = ((float)(dividerReading - dividerMinReading) / (dividerRange)) * 8;
   batteryLevel = (byte)batteryLevelFloat;
   batteryLevel = constrain(batteryLevel,0,7);
-//  Serial.println(batteryLevel);
 }
 
-
 void setupBatteryMonitor() {
-  pinMode(PIN_BATTERY_MONITOR, INPUT);
-  pinMode(PIN_BATTERY_INDICATOR, OUTPUT);
+  pinMode(pinBatteryMonitor, INPUT);
   ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
   ADCSRA |= bit (ADPS2);  // set ADC prescaler to 16 (from default 128)
   for (byte i = 0; i < 30; i++) {
     calculateBatteryLevel();
   }
 }
-
-
-// update the data being sent back to the transmitter, and if battery low then turn on the low battery LED
-// make it blink instead?
-//void updateBatteryIndicator(){
-////  updateAckStatusForTx(5, batteryLevel);  // battery level will be shown in bits 5/6/7 in status
-//  if(batteryLevel<=0){
-////    digitalWrite(PIN_BATTERY_INDICATOR,HIGH);
-//  }
-//}
-
-
-//void setupBatteryMonitor(){
-//  pinMode(PIN_BATTERY_MONITOR,INPUT);
-//  pinMode(PIN_BATTERY_INDICATOR,OUTPUT);
-//  for(byte i = 0; i < 20; i++){
-//    calculateBatteryVoltage();
-//    calculateBatteryLevel();
-//  }
-//  updateBatteryIndicator();
-//}
-
-
-
 
