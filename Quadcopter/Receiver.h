87a6d2bd34c9eefbe9d2ea12a1c33a34b85ee221
Matt <matthedley@hotmail.co.uk>
2018-01-18 17:02:28 +0000

    reformat

--- Quadcopter/Receiver.h
+++ Quadcopter/Receiver.h
@@ -3,9 +3,9 @@
 // bit 1:
 // bit 2: 1 = Attitude mode, 0 = rate mode
 // bit 3: 1 = auto-level on, 0 = auto-level off
-// bit 4: 
-// bit 5: 
-// bit 6: 
+// bit 4:
+// bit 5:
+// bit 6:
 // bit 7; 1 = TURN OFF MOTORS - reset required for re-enable // not implemented on current Tx
 
 // Acknowledgement byte
@@ -13,17 +13,17 @@
 // bit 1: 1 = OK
 // bit 2: 1 = some error
 // bit 3:
-// bit 4: 
+// bit 4:
 // bits 5/6/7: battery indicator (0-7)
 
 const byte address[6] = "1Node";
 const byte pipeNumber = 1;
-RF24 radio(9,10); // CE, CSN (SPI SS)
+RF24 radio(9, 10); // CE, CSN (SPI SS)
 
 byte statusForAck = 0; // send this back to transmitter as acknowledgement package
 const byte OK = 1;
 
-struct dataStruct{
+struct dataStruct {
   byte throttle;
   byte roll;
   byte pitch;
@@ -42,7 +42,7 @@ bool rxHeartbeat = false;
 unsigned long lastRxReceived = 0;
 const unsigned long heartbeatTimeout = 500;
 
-byte calculateCheckSum(){
+byte calculateCheckSum() {
   byte sum = 0;
   sum += rcPackage.throttle;
   sum += rcPackage.pitch;
@@ -54,15 +54,15 @@ byte calculateCheckSum(){
   return sum;
 }
 
-void printPackage(){
-  Serial.print(rcPackage.throttle);Serial.print('\t');
-  Serial.print(rcPackage.roll);Serial.print('\t');
-  Serial.print(rcPackage.pitch);Serial.print('\t');
-  Serial.print(rcPackage.yaw);Serial.print('\t');
-  Serial.print(rcPackage.control);Serial.print('\t');
-  Serial.print(rcPackage.alive);Serial.print('\t');
-  Serial.print(rcPackage.checksum);Serial.print('\t');
-  Serial.print("CHKSUM_DIFF: ");Serial.println(rcPackage.checksum - calculateCheckSum());
+void printPackage() {
+  Serial.print(rcPackage.throttle); Serial.print('\t');
+  Serial.print(rcPackage.roll); Serial.print('\t');
+  Serial.print(rcPackage.pitch); Serial.print('\t');
+  Serial.print(rcPackage.yaw); Serial.print('\t');
+  Serial.print(rcPackage.control); Serial.print('\t');
+  Serial.print(rcPackage.alive); Serial.print('\t');
+  Serial.print(rcPackage.checksum); Serial.print('\t');
+  Serial.print("CHKSUM_DIFF: "); Serial.println(rcPackage.checksum - calculateCheckSum());
 }
 
 void setupRadio() {
@@ -71,45 +71,44 @@ void setupRadio() {
   radio.setPALevel(RF24_PA_HIGH);  // MIN, LOW, HIGH, MAX
   radio.enableAckPayload();
   radio.enableDynamicPayloads();
-// RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps // slower is more reliable and gives longer range
+  // RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps // slower is more reliable and gives longer range
   radio.setDataRate(RF24_250KBPS);
-//   * @param delay How long to wait between each retry, in multiples of 250us,
-//   * max is 15.  0 means 250us, 15 means 4000us.
-//   * @param count How many retries before giving up, max 15
-//  radio.setRetries();   // default is setRetries(5,15) // note restrictions due to ack payload
+  //   * @param delay How long to wait between each retry, in multiples of 250us,
+  //   * max is 15.  0 means 250us, 15 means 4000us.
+  //   * @param count How many retries before giving up, max 15
+  //  radio.setRetries();   // default is setRetries(5,15) // note restrictions due to ack payload
   radio.openReadingPipe(pipeNumber, address);
   radio.startListening();
 }
 
-void updateAckStatusForTx(){
+void updateAckStatusForTx() {
   statusForAck = 0;
   statusForAck |= batteryLevel << 5;
   statusForAck |= OK << 1; // obviously need to change if not ok
   statusForAck |= 1; // set low bit to 1 always
-//  Serial.print('\t');Serial.println(statusForAck);
+  //  Serial.print('\t');Serial.println(statusForAck);
 }
 
 bool checkRadioForInput() {
   if ( radio.available()) {
-      radio.read( &rcPackage, sizeof(rcPackage) );
+    radio.read( &rcPackage, sizeof(rcPackage) );
     // load acknowledgement payload for the next transmission (first transmission will not get any ack payload (but will get normal ack))
-    radio.writeAckPayload(1,&statusForAck,sizeof(statusForAck));
-    if(rcPackage.checksum != calculateCheckSum()){
+    radio.writeAckPayload(1, &statusForAck, sizeof(statusForAck));
+    if (rcPackage.checksum != calculateCheckSum()) {
       radio.flush_rx();
       return false;
     }
-//    Serial.println(statusForAck);
     lastRxReceived = millis();
-    radio.flush_rx(); // probably remove
+    radio.flush_rx();
     updateAckStatusForTx(); // for next time
     return true;
   }
   return false;
 }
 
-bool checkHeartbeat(){
-  if(millis()-lastRxReceived > heartbeatTimeout){
-//    Serial.println(F("Lost connection"));
+bool checkHeartbeat() {
+  if (millis() - lastRxReceived > heartbeatTimeout) {
+    //    Serial.println(F("Lost connection"));
     rxHeartbeat = false;
   }
   else {
@@ -118,25 +117,25 @@ bool checkHeartbeat(){
   return rxHeartbeat;
 }
 
-void mapThrottle(int *throttle){
-  if (rcPackage.throttle < 12){
+void mapThrottle(int *throttle) {
+  if (rcPackage.throttle < 12) {
     *throttle = 0;
   }
   else {
-    *throttle = map(rcPackage.throttle,0,255,THROTTLE_MIN_SPIN,THROTTLE_LIMIT);
+    *throttle = map(rcPackage.throttle, 0, 255, THROTTLE_MIN_SPIN, THROTTLE_LIMIT);
   }
 }
 
 void mapRcToPidInput(float *roll, float *pitch, float *yaw, bool mode) {
   if (!mode) {
-    *roll = (float)map(rcPackage.roll+1, 0,255, rateMin, rateMax);
-    *pitch = (float)map(rcPackage.pitch+1, 0,255, rateMin, rateMax);
-    *yaw = (float)map(rcPackage.yaw+1, 0,255, rateMax, rateMin);
+    *roll = (float)map(rcPackage.roll + 1, 0, 255, rateMin, rateMax);
+    *pitch = (float)map(rcPackage.pitch + 1, 0, 255, rateMin, rateMax);
+    *yaw = (float)map(rcPackage.yaw + 1, 0, 255, rateMax, rateMin);
   }
   else {
-    *roll = (float)map(rcPackage.roll+1, 0,255, attitudeMin, attitudeMax);
-    *pitch = (float)map(rcPackage.pitch+1, 0,255, attitudeMin, attitudeMax);
-    *yaw = (float)map(rcPackage.yaw+1, 0,255, attitudeMin, attitudeMax);
+    *roll = (float)map(rcPackage.roll + 1, 0, 255, attitudeMin, attitudeMax);
+    *pitch = (float)map(rcPackage.pitch + 1, 0, 255, attitudeMin, attitudeMax);
+    *yaw = (float)map(rcPackage.yaw + 1, 0, 255, attitudeMin, attitudeMax);
   }
 }
 
